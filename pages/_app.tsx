2764dfd488ba9aa6d08b5446c101f9842069d199
Hum Bruno <hsbruno1@gmail.com>
2022-11-21 21:52:21 +0000

    chore - project setup files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.scss';
+import 'styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="description" content="Create value to your business." />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
